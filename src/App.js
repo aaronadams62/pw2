29fa9fd07feefad28848f341994b9bf0300e2571
aaronadams62 <aaron_adams62@yahoo.com>
2026-02-25 15:35:30 -0500

    fix: resolve #69 and prep #75 firebase hosting phase 1

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
 
   return (
     <ErrorBoundary>
-      <Router>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={<MainSite />} />
           <Route path="/admin" element={<AdminLogin />} />
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// react-slick/enquire.js expects matchMedia in the test environment.
+if (!window.matchMedia) {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => false,
+    }),
+  });
+}
